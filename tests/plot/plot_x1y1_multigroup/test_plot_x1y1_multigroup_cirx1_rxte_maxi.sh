bb5bca40ef873b20acc0c46942de8329e04002f3
tenoto <teruaki.enoto@gmail.com>
2018-08-21 11:48:22 +0900

    example: MAXI and RXTE plot

---
+++ tests/plot/plot_x1y1_multigroup/test_plot_x1y1_multigroup_cirx1_rxte_maxi.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -f
+
+cd $HOPPY_PATH/tests/plot/plot_x1y1_multigroup
+
+$HOPPY_PATH/hoppy/plot/plot_x1y1_multigroup.py \
+	$HOPPY_PATH/tests/plot/plot_x1y1_multigroup/data/plot_x1y1_multigroup_setup_cirx1_rxte_maxi.yaml
