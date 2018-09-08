260c09b39d91a955235d4ef15888f95d74b4a4b5
Teruaki Enoto <teruaki.enoto@gmail.com>
2018-09-08 03:35:49 -0400

    luminosity calculation

--- hoppy/physics/hoppy_physics.py
+++ hoppy/physics/hoppy_physics.py
@@ -1,3 +1,10 @@
 def get_luminosity(flux,d_kpc):
-	luminosity = 1.2e+32 * (flux / 1e-12) * d_kpc**2 
+	luminosity = 1.2e+32 * (flux / 1e-12) * d_kpc**2 # erg/s
 	return luminosity
+
+def get_spindown_luminosity(period, pdot, moment_of_inertia=1.0):
+	"""
+	moment of inertia normalized to I = 1e+45 g cm2
+	"""
+	spindown_luminosity = 3.94e+35 * moment_of_inertia / period**3 * (pdot/1e-11)
+	return spindown_luminosity
