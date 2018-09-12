58166fd71ad8bc110e11641a8ace0b674d29cf82
Teruaki Enoto <teruaki.enoto@gmail.com>
2018-09-12 14:35:48 -0400

    pulse profiles

--- hoppy/plot/fplot_pulseprofile.py
+++ hoppy/plot/fplot_pulseprofile.py
@@ -1,125 +1,148 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-__name__    = 'fplot_pulseprofile'
-__author__  = 'Teruaki Enoto'
-__version__ = '1.00'
-__date__    = '2018 March 30'
-
-import os
+import os 
 import sys
-#import pyfits
-import astropy.io.fits as pyfits
-import subprocess
-from optparse import OptionParser
+import argparse
+import astropy.io.fits as pyfits 
 
-parser = OptionParser()
-parser.add_option("-i","--inputevtfits",dest="inputevtfits",
-	action="store",help="input event fits file, PHASE column is required",type="string")
-parser.add_option("-o","--outputhistfits",dest="outputhistfits",
-	action="store",help="output histogram fits file",type="string")
-parser.add_option("-n","--nbin",dest="nbin",
-	action="store",help="Phase bin for a one pulse",type="int")
-parser.add_option("-c","--column",dest="column",default="PULSE_PHASE",
-	action="store",help="Column name for PHASE (Defaul=PULSE_PHASE)",type="string")
-parser.add_option("-t","--title",dest="title",default="",
-	action="store",help="title",type="string")
-parser.add_option("-b","--backgroundrate",dest="backgroundrate",default=0.0,
-	action="store",help="Background rate (cps)",type="float")
-(options, args) = parser.parse_args()
+__author__  = 'Teruaki Enoto'
+__version__ = '1.01'
+__date__    = '2018 September 12'
 
-if options.inputevtfits == None:
-	print "input event fits file is needed. %> -i inputevtfits"
-	quit()
-if options.outputhistfits == None:	
-	print "output histogram fits file is needed. %> -o outputhistfits"
-	quit()
-if options.nbin == None:	
-	print "Phase bin for a one pulse is needed. %> -n nbin"
-	quit()
+def fplot_pulseprofile(inputfits,outfits=None,
+		expr=None,nbin=20,colname="PULSE_PHASE",
+		bgdrate=0.0,title=''):
+	dump  = "inputfits: %s\n" % inputfits
+	if outfits == None:
+		if os.path.splitext(inputfits)[-1] == '.evt':
+			outfits = os.path.basename(inputfits).replace('.evt','_pls.fits')
+		elif os.path.splitext(inputfits)[-1] == '.fits':
+			outfits = os.path.basename(inputfits).replace('.fits','_pls.fits')
+	dump += "outfits: %s\n" % outfits
+	dump += "expr: %s\n" % expr 
+	dump += "nbin: %d\n" % nbin
+	dump += "colname: %s\n" % colname
+	dump += "bgdrate: %.4e\n" % bgdrate
+	dump += 'title: %s\n' % title
+	print(dump)
 
-print "input event fits file  : %s " % options.inputevtfits
-print "output event fits file : %s " % options.outputhistfits	
-print "nbin : %.2f " % options.nbin
+	if not os.path.exists(inputfits):
+		sys.stderr.write('error: file %s does not exist.\n' % inputfits)
+		exit()
+	if os.path.exists(outfits):
+		sys.stderr.write('error: file %s has already existed.\n' % outfits)
+		exit()
 
-if not os.path.exists(options.inputevtfits):
-	print "input event fits file does not exists: %s" % options.inputevtfits
-	quit()
-if os.path.exists(options.outputhistfits):
-	print "output histogram fits file has already existed: %s " % options.outputhistfits
-	quit()
+	cmd = 'rm -f tmp01_fselect.evt;\n'
+	if expr != None:
+		cmd += 'fselect %s tmp01_fselect.evt "%s"' % (inputfits,expr)
+	else:
+		cmd += 'ln -s %s tmp01_fselect.evt' % (inputfits)
+	print(cmd);os.system(cmd)
 
-basename = os.path.splitext(options.outputhistfits)[0]
+	binwidth = 1.0/float(nbin)
+	cmd  = 'rm -f tmp02_fhisto.evt;\n'
+	cmd += 'fhisto tmp01_fselect.evt tmp02_fhisto.evt '
+	cmd += '%s %.5f ' % (colname, binwidth)
+	cmd += 'lowval=0.0 highval=1.0 outcolx=%s outcoly=COUNTS outcolz=ERROR extname=PROFILE' % colname
+	print(cmd);os.system(cmd)
 
-tmp1 = '%s_tmp1.fht' % basename
-cmd  = 'rm -f %s' % tmp1
-print(cmd);os.system(cmd)
+	cmd  = 'rm -f tmp03_fhisto_copy.evt;\n'
+	cmd += 'fcalc clobber=yes tmp02_fhisto.evt tmp03_fhisto_copy.evt '
+	cmd += '%s %s+1 ' % (colname,colname)
+	print(cmd);os.system(cmd)
 
-phase_bin = 1.0/float(options.nbin)
-cmd  = 'fhisto %s %s ' % (options.inputevtfits,tmp1)
-cmd += '%s %.3f ' % (options.column, phase_bin)
-cmd += 'lowval=0.0 highval=1.0 outcolx=%s outcoly=COUNTS outcolz=ERROR extname=PROFILE' % options.column
-print(cmd);os.system(cmd)
+	cmd  = 'rm -f tmp04_ftmerge.evt;\n'
+	cmd += 'ftmerge tmp02_fhisto.evt,tmp03_fhisto_copy.evt tmp04_ftmerge.evt'
+	print(cmd);os.system(cmd)
 
-tmp2 = '%s_tmp2.fht' % basename
-cmd  = 'fcalc clobber=yes %s %s %s %s+1.0' % (tmp1,tmp2,options.column,options.column)
-print(cmd);os.system(cmd)
+	hdu = pyfits.open(inputfits)
+	exposure = float(hdu[1].header['EXPOSURE'])
+	cmd  = 'fparkey %.5f tmp04_ftmerge.evt[1] EXPOSURE add=yes;\n' % exposure
+	cmd += 'fcalc tmp04_ftmerge.evt %s RATE "COUNTS/(#EXPOSURE/((float) %d ))"\n' % (outfits,nbin)
+	cmd += 'fcalc %s %s RATE_ERR "sqrt(COUNTS)/(#EXPOSURE/((float) %d ))" clobber=YES;' % (outfits,outfits,nbin)
+	print(cmd);os.system(cmd)
 
-tmp3 = '%s_tmp3.fht' % basename
-cmd = 'ftmerge %s,%s %s' % (tmp1,tmp2,tmp3)
-print(cmd);os.system(cmd)
+	hdu = pyfits.open(outfits)
+	rate_list = hdu['PROFILE'].data['RATE'][0:nbin]
+	max_rate = max(rate_list)
+	min_rate = min(rate_list)
+	bgdsub_max_rate = max_rate - bgdrate
+	bgdsub_min_rate = min_rate - bgdrate
+	pulsed_fraction = (bgdsub_max_rate-bgdsub_min_rate)/(bgdsub_max_rate+bgdsub_min_rate)
+	cmd  = 'fparkey %.5f %s[1] MAX_WBGD add=yes comm="Maximum rate (cps) before background subtraction.";\n' % (max_rate,outfits)
+	cmd += 'fparkey %.5f %s[1] MIN_WBGD add=yes comm="Minimum rate (cps) before background subtraction.";\n' % (min_rate,outfits)
+	cmd += 'fparkey %.5f %s[1] MAX_RATE add=yes comm="Background-subtracted maximum rate (cps).";\n' % (bgdsub_max_rate,outfits)
+	cmd += 'fparkey %.5f %s[1] MIN_RATE add=yes comm="Background-subtracted minimum rate (cps).";\n' % (bgdsub_min_rate,outfits)
+	cmd += 'fparkey %.5f %s[1] PULFRACT add=yes comm="Pulsed fraction after the background subtraction.";\n' % (pulsed_fraction,outfits)
+	cmd += 'fparkey %d %s[1] NBINPHAS add=yes comm="Number of phase bins";\n' % (nbin,outfits)
+	cmd += 'fparkey %.5e %s[1] BACKRATE add=yes comm="Subtracted background rate";\n' % (bgdrate,outfits)
+	print(cmd);os.system(cmd)
+	
+	subtitle = 'BGD=%.2e (cps) Max=%.2e Min=%.2e Pulsed_Fraction=%.1f%%' % (bgdrate,
+		bgdsub_max_rate,bgdsub_min_rate,pulsed_fraction*100.0)
+	psfile = '%s.ps' % os.path.splitext(outfits)[0]
+	cmd  = 'fplot %s %s "RATE[RATE_ERR]" - /xw @ ' % (outfits,colname)
+	cmd += '<<EOF\n'
+	cmd += 'line step on\n'
+	cmd += 'time off\n'
+	cmd += 'lwid 5 on\n'
+	cmd += 'lwid 5\n'
+	cmd += 'la t %s\n' % title
+	cmd += 'la f %s\n' % subtitle 
+	cmd += 'we %s\n' % os.path.splitext(os.path.basename(psfile))[0]
+	cmd += 'hard %s/cps\n' % psfile
+	cmd += 'quit\n'
+	cmd += 'EOF\n'
+	print(cmd);os.system(cmd)
 
-cmd = 'get_total_gti_exposure.py %s' % options.inputevtfits
-dump = subprocess.Popen(cmd.split(' '),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-exposure = float(dump.stdout.readlines()[0].strip())
-cmd  = 'fparkey %.5f %s[1] EXPOSURE add=yes\n' % (exposure,tmp3)
-cmd += 'fcalc %s %s RATE "COUNTS/(#EXPOSURE/((float) %d ))"\n' % (tmp3,options.outputhistfits,options.nbin)
-#cmd += 'fcalc clobber=yes %s %s RATE_ERR "sqrt(COUNTS)/(#EXPOSURE/((float) %d ))" ' % (tmp3,options.outputhistfits,options.nbin)
-cmd += 'fcalc %s %s RATE_ERR "sqrt(COUNTS)/(#EXPOSURE/((float) %d ))" clobber=YES;' % (options.outputhistfits,options.outputhistfits,options.nbin)
-print(cmd);os.system(cmd)
+	pdffile = '%s.pdf' % os.path.splitext(os.path.basename(psfile))[0]
+	outdir = os.path.dirname(outfits)
+	cmd  = 'ps2pdf %s ;' % (psfile)
+	if outdir == '':
+		cmd += 'mv %s . ' % pdffile
+	else:	
+		cmd += 'mv %s %s ' % (pdffile,outdir)
+	print(cmd);os.system(cmd)
 
-hdu = pyfits.open(options.outputhistfits)
-rate_list = hdu['PROFILE'].data['RATE'][0:options.nbin]
-max_rate = max(rate_list)
-min_rate = min(rate_list)
-bgdsub_max_rate = max_rate - options.backgroundrate
-bgdsub_min_rate = min_rate - options.backgroundrate
-pulsed_fraction = (bgdsub_max_rate-bgdsub_min_rate)/(bgdsub_max_rate+bgdsub_min_rate)
-cmd  = 'fparkey %.5f %s[1] MAX_WBGD add=yes comm="Maximum rate (cps) before background subtraction.";\n' % (max_rate,options.outputhistfits)
-cmd += 'fparkey %.5f %s[1] MIN_WBGD add=yes comm="Minimum rate (cps) before background subtraction.";\n' % (min_rate,options.outputhistfits)
-cmd += 'fparkey %.5f %s[1] MAX_RATE add=yes comm="Background-subtracted maximum rate (cps).";\n' % (bgdsub_max_rate,options.outputhistfits)
-cmd += 'fparkey %.5f %s[1] MIN_RATE add=yes comm="Background-subtracted minimum rate (cps).";\n' % (bgdsub_min_rate,options.outputhistfits)
-cmd += 'fparkey %.5f %s[1] PULFRACT add=yes comm="Pulsed fraction after the background subtraction.";\n' % (pulsed_fraction,options.outputhistfits)
-cmd += 'fparkey %d %s[1] NBINPHAS add=yes comm="Number of phase bins";\n' % (options.nbin,options.outputhistfits)
-cmd += 'fparkey %.5e %s[1] BACKRATE add=yes comm="Subtracted background rate";\n' % (options.backgroundrate,options.outputhistfits)
-print(cmd);os.system(cmd)
-subtitle = 'BGD=%.2e (cps) Max=%.2e Min=%.2e Pulsed_Fraction=%.1f%%' % (options.backgroundrate,
-	bgdsub_max_rate,bgdsub_min_rate,pulsed_fraction*100.0)
+	for tmpfile in ['tmp01_fselect.evt','tmp02_fhisto.evt','tmp03_fhisto_copy.evt','tmp04_ftmerge.evt',psfile]:
+		cmd = 'rm -f %s' % tmpfile
+		print(cmd);os.system(cmd	)
 
-psfile = '%s.ps' % os.path.splitext(options.outputhistfits)[0]
-cmd  = 'fplot %s %s "RATE[RATE_ERR]" - /xw @ ' % (options.outputhistfits,options.column)
-cmd += '<<EOF\n'
-cmd += 'line step on\n'
-cmd += 'time off\n'
-cmd += 'lwid 5 on\n'
-cmd += 'lwid 5\n'
-cmd += 'la t %s\n' % options.title
-cmd += 'la f %s\n' % subtitle 
-cmd += 'hard %s/cps\n' % psfile
-cmd += 'quit\n'
-cmd += 'EOF\n'
-print(cmd);os.system(cmd)
+if __name__=="__main__":
 
-pdffile = '%s.pdf' % os.path.splitext(os.path.basename(psfile))[0]
-outdir = os.path.dirname(options.outputhistfits)
-cmd  = 'ps2pdf %s ;' % (psfile)
-if outdir == '':
-	cmd += 'mv %s . ' % pdffile
-else:	
-	cmd += 'mv %s %s ' % (pdffile,outdir)
-print(cmd);os.system(cmd)
+	parser = argparse.ArgumentParser(
+		prog='fplot_pulseprofile.py',
+		usage='fplot_pulseprofile.py input.fits',
+		description='Plot a pulse profile from fits file.',
+		epilog='',
+		add_help=True)
 
-cmd = 'rm -f %s %s %s %s' % (tmp1,tmp2,tmp3,psfile)
-print(cmd);os.system(cmd)
-
-
+	parser.add_argument(
+		'inputfits',metavar='inputfits',type=str,
+		help='Input fitsfile') 	
+	parser.add_argument(
+		'--outfits',metavar='outfits',type=str, default=None,
+		help='Output fitsfile name') 				
+	parser.add_argument(
+		'--expr',metavar='expr',type=str, default=None,
+		help='Filter expression for fselect.') 		
+	parser.add_argument(
+		'--nbin',metavar='nbin',type=int, default=20,
+		help='Folding number of bins.') 
+	parser.add_argument(
+		'--colname',metavar='colname',type=str,default='PULSE_PHASE',
+		help='Phase column name of fits file.') 
+	parser.add_argument(
+		'--bgdrate',metavar='bgdrate',type=float,default=0.0,
+		help='Background rate (cps).') 	
+	parser.add_argument(
+		'--title',metavar='title',type=str,default='Pulse Profile',
+		help='Title for output figure.') 								
+	args = parser.parse_args()	
+	
+	fplot_pulseprofile(args.inputfits,outfits=args.outfits,
+		expr=args.expr,nbin=args.nbin,colname=args.colname,
+		bgdrate=args.bgdrate,title=args.title)
 
