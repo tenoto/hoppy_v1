9eb36bf6bfd395581d16631363c705191bfdb67c
Teruaki Enoto <teruaki.enoto@gmail.com>
2019-05-29 10:31:50 +0900

    xspec fit

---
+++ tests/xspec/test_xspec_multi_observations.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -f 
+
+rm -rf out/multi; mkdir -p out/multi
+
+$HOPPY_PATH/hoppy/xspec/make_csv2xspec.py \
+	data/filelist2csv.txt \
+	data/xspec_multifit_param.yaml \
+	out/multi/xspec_multifit.csv
+
+$HOPPY_PATH/hoppy/xspec/xspec_multi_observations.py \
+	out/multi/xspec_multifit.csv \
+	data/xspec_multifit_param.yaml 
+
+
+
+
