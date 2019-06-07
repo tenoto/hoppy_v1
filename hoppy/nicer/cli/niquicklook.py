7e27e67cea62f2b3490ab7e84c57ed6f34c6ab10
tenoto <teruaki.enoto@gmail.com>
2019-06-07 10:43:55 +0900

    update niquicklook.pdf

--- hoppy/nicer/cli/niquicklook.py
+++ hoppy/nicer/cli/niquicklook.py
@@ -4,6 +4,7 @@ import os
 import sys 
 import numpy as np
 import astropy.io.fits as pyfits
+from astropy.stats import LombScargle
 import matplotlib.pyplot as plt 
 
 plt.style.use('https://raw.githubusercontent.com/tenoto/macsettings/master/supermongo.mplstyle')
@@ -13,6 +14,13 @@ def wget_nicer_cleanevt(obsid,yyyy_mm):
 	clevt_path = 'https://heasarc.gsfc.nasa.gov/FTP/nicer/data/obs/%s/%s/xti/event_cl/ni%s_0mpu7_cl.evt.gz' % (yyyy_mm,obsid,obsid)
 	cmd = 'wget -q -nH --no-check-certificate --cut-dirs=8 -r -l0 -c -N -np -R \'index*\' -erobots=off --retr-symlinks %s' % clevt_path
 	filename = 'ni%s_0mpu7_cl.evt.gz' % obsid
+	print(cmd);os.system(cmd)
+	return filename
+
+def wget_nicer_orbfile(obsid,yyyy_mm):
+	orbfile_path = 'https://heasarc.gsfc.nasa.gov/FTP/nicer/data/obs/%s/%s/auxil/ni%s.orb.gz' % (yyyy_mm,obsid,obsid)
+	cmd = 'wget -q -nH --no-check-certificate --cut-dirs=8 -r -l0 -c -N -np -R \'index*\' -erobots=off --retr-symlinks %s' % orbfile_path
+	filename = 'ni%s.orb.gz' % obsid
 	print(cmd);os.system(cmd)
 	return filename
 
@@ -30,20 +38,28 @@ def pi2keV(pi,instrume):
 	keV = a * pi + b
 	return keV
 
+def get_lightcurve(times,tbin=1.):
+	nbin = int(round(times[-1] - times[0])/float(tbin))
+	ncounts, bins_edge = np.histogram(times-times[0],bins=nbin)
+	times = 0.5*(bins_edge[1:] + bins_edge[:-1])	
+	terr = 0.5*(bins_edge[1:] - bins_edge[:-1])	
+	nerr = np.sqrt(ncounts)
+	return times, ncounts, terr, nerr
+
 def plot(x,y,xerr=None,yerr=None,xlabel='X',ylabel='Y',
 	xlim=None,ylim=None,xlog=False,ylog=False,
 	legend='',title='',pdfname='hoge.pdf'):
 	plt.clf()
 	plt.figure(figsize=(9,6))
-	plt.errorbar(x,y,xerr=xerr,yerr=yerr,drawstyle='steps-mid',marker='',color='k',ecolor='k')
-	plt.xlabel(xlabel);plt.ylabel(ylabel)
-	plt.title(title)
+	plt.errorbar(x,y,xerr=xerr,yerr=yerr,drawstyle='steps-mid',marker='',color='k',ecolor='k',rasterized=True)
+	plt.xlabel(xlabel);plt.ylabel(ylabel);plt.title(title)
 	if xlim != None: plt.xlim(xlim)
 	if ylim != None: plt.ylim(ylim)	
 	if xlog: plt.xscale('log')
 	if ylog: plt.yscale('log')	
 	plt.legend([legend])	
 	plt.savefig(pdfname)
+	return pdfname
 
 class EVTFILE():
 	def __init__(self,filename):
@@ -64,8 +80,10 @@ class EVTFILE():
 		self.date_end = self.hdu[0].header['DATE-END'] 
 		self.instrume = self.hdu[0].header['INSTRUME']
 		self.object = self.hdu[0].header['OBJECT']
-		
+		self.ra_obj = self.hdu[0].header['RA_OBJ']
+		self.dec_obj = self.hdu[0].header['DEC_OBJ']
 		self.exposure = self.hdu['EVENTS'].header['EXPOSURE']
+
 		self.nevents = len(self.hdu['EVENTS'].data)	 
 		self.ngtis	 = len(self.hdu['GTI'].data)
 		if self.exposure > 0.0:
@@ -73,6 +91,7 @@ class EVTFILE():
 		else:
 			self.rate = 0.0
 
+		self.title = '%s Obs#%s %s' % (self.object,self.obs_id,self.date_obs)
 		self.pdf_list = []
 
 	def show_property(self):
@@ -122,12 +141,11 @@ class EVTFILE():
 		
 		fig, ax = plt.subplots(figsize=(12,8))		
 		for i in range(len(curves)):			
-			plt.errorbar(curves[i][0],curves[i][1]/time_bin,yerr=np.sqrt(curves[i][1])/time_bin,
-									 label='%.1f-%.1f keV' % (ebands[i][0],ebands[i][1]),
-									 marker='',drawstyle='steps-mid',color=color[i],ecolor=color[i])			
-			
-		#title = '%s/%s %s ObsID=%s %s (%.1f s)' % (self.telescop, self.instrume, self.object, self.obs_id,self.date_obs,self.exposure)
-		title = '%s ObsID=%s %s (Texp=%.1f s) %.1f-s-bin' % (self.object, self.obs_id,self.date_obs,self.exposure,time_bin)
+			plt.errorbar(curves[i][0],curves[i][1]/time_bin,
+				yerr=np.sqrt(curves[i][1])/time_bin,
+				label='%.1f-%.1f keV' % (ebands[i][0],ebands[i][1]),
+				marker='',drawstyle='steps-mid',color=color[i],ecolor=color[i])			
+		title = '%s (T=%.1fs, bin=%.1fs)' % (self.title,self.exposure,time_bin)
 		ax.set_xlim(curves[0][0][0]-0.5*time_bin,curves[0][0][-1]+0.5*time_bin)
 		ax.set_xlabel('Time (sec)')
 		ax.set_ylabel('Count rate (cps)')
@@ -155,28 +173,7 @@ class EVTFILE():
 			spectrum_error = np.sqrt(binned_counts)/(2.0*binned_widths)			
 		return binned_centers,sepctrum,binned_widths,spectrum_error
 
-		"""
-		fig, ax = plt.subplots(figsize=(12,8))	 
-		if flag_exposure:
-			plt.errorbar(binned_centers,binned_counts/(2.0*binned_widths)/exposure,
-				yerr=np.sqrt(binned_counts)/(2.0*binned_widths)/exposure,
-				marker='',drawstyle='steps-mid',color='k',ecolor='k')
-			ax.set_ylabel('Count / sec / keV')			
-		else:
-			plt.errorbar(binned_centers,binned_counts/(2.0*binned_widths),
-				yerr=np.sqrt(binned_counts)/(2.0*binned_widths),
-				marker='',drawstyle='steps-mid',color='k',ecolor='k')
-			ax.set_ylabel('Count / keV')			
-		ax.set_yscale('log')		
-		ax.set_xscale('log')
-		ax.set_xlabel('Energy (keV)')
-
-		plt.savefig(pdfname)
-		"""		
-
 	def set_rebinned_gti(self,separation_threshold=100.0):
-		#for gti in self.hdu['GTI'].data:
-		#	print(gti['START'],gti['STOP'],gti['STOP']-gti['START'])		
 		bingti_start = []
 		bingti_stop = []	
 		bingti_exposure = []	
@@ -204,13 +201,25 @@ class EVTFILE():
 		self.bingti_stop = np.array(bingti_stop)				
 		self.bingti_exposure = np.array(bingti_exposure)
 
+	def plot_spectrum(self):
+		x,y,xerr,yerr = self.get_spectrum(
+			pi2keV(self.hdu['EVENTS'].data['PI'],instrume='XTI'),
+			exposure=self.exposure,flag_exposure=True)
+		pdfname = 'ni%s_avespec.pdf' % (self.obs_id)
+
+		title = '%s (T=%.1fs)' % (self.title,self.exposure)		
+		plot(x,y,xerr=xerr,yerr=yerr,xlabel='Energy (keV)',ylabel='Count/sec/keV',
+			xlim=(0.2,15.0),title=title,
+			xlog=True,ylog=True,legend='NICER XTI',pdfname=pdfname)
+		self.pdf_list.append(pdfname)
+
 	def plot_spectrum_gti(self):
 		self.set_rebinned_gti(self)
 		tzero = self.hdu['EVENTS'].data['TIME'][0] 
 
 		pdfname = 'ni%s_gtispec.pdf' % (self.obs_id)
 		self.pdf_list.append(pdfname)
-		cmd  = 'gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite '
+		cmd	= 'gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite '
 		cmd += '-sOutputFile=%s ' % pdfname
 		for i in range(len(self.bingti_start)):
 			tstart = self.bingti_start[i]
@@ -224,8 +233,7 @@ class EVTFILE():
 				exposure=texp,flag_exposure=True)
 			pdfname = 'tmp_gti%02d_spec.pdf' % i
 
-			title = '%s ObsID=%s GTI-%02d (%.1f...%.1f Texp=%.1f s)' % (
-				self.object,self.obs_id,i,tstart-tzero,tstop-tzero,texp)
+			title = '%s GTI-%02d (%.1f...%.1f Texp=%.1f s)' % (self.title,i,tstart-tzero,tstop-tzero,texp)
 			plot(x,y,xerr=xerr,yerr=yerr,xlabel='Energy (keV)',ylabel='Count/sec/keV',
 				xlim=(0.2,15.0),ylim=(0.02,500.0),title=title,
 				xlog=True,ylog=True,legend='NICER XTI',pdfname=pdfname)
@@ -235,7 +243,7 @@ class EVTFILE():
 
 	def pdf_merge(self):
 		pdfname = 'ni%s_ql.pdf' % (self.obs_id)		
-		cmd  = 'gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite '
+		cmd	= 'gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite '
 		cmd += '-sOutputFile=%s ' % pdfname
 		for pdf in self.pdf_list:
 			cmd += '%s ' % pdf 
@@ -243,19 +251,76 @@ class EVTFILE():
 		for pdf in self.pdf_list:
 			os.system('rm -f %s' % pdf)
 
-def run(obsid,yyyy_mm,flag_download=True):
+	def run_barycorr(self,orbfile,ra=None,dec=None,ephem=None):
+		sys.stdout.write('=== %s ===\n' % sys._getframe().f_code.co_name)
+
+		if ra == None: ra = self.ra_obj
+		if dec == None: dec = self.dec_obj
+		self.baryevt = "%s_bary.evt" % os.path.splitext(self.filename.replace('.gz',''))[0]
+		self.barylog = "%s.log" % os.path.splitext(self.baryevt)[0]
+		cmd	= 'barycorr ra=%.6f dec=%.6f ' % (ra,dec)
+		cmd += 'infile=%s ' % self.filename
+		cmd += 'outfile=%s ' % self.baryevt
+		cmd += 'orbitfiles=%s ' % orbfile
+		if ephem == None:
+			cmd += 'refframe=ICRS	ephem=JPLEPH.430 '
+		else:
+			cmd += '%s ' % ephem 
+		cmd += ">& %s " % self.barylog
+		print(cmd); os.system(cmd)
+
+	def plot_LombScargle(self,lower_keV=0.8,upper_keV=5.0,tbin=0.5):
+		sys.stdout.write('=== %s ===\n' % sys._getframe().f_code.co_name)
+
+		lower_pi = keV2pi(lower_keV,instrume=self.instrume)
+		upper_pi = keV2pi(upper_keV,instrume=self.instrume)		
+
+		self.hdu_bary =	pyfits.open(self.baryevt)
+		evttimes = self.hdu_bary['EVENTS'].data['TIME']		
+		filter_energy = np.logical_and(self.hdu_bary['EVENTS'].data['PI'] >= lower_pi, self.hdu_bary['EVENTS'].data['PI'] <= upper_pi)
+		time_series = self.hdu_bary['EVENTS'].data['TIME'][filter_energy]
+
+		lc_x,lc_y,lc_xerr,lc_yerr=get_lightcurve(time_series,tbin=tbin)
+		title = '%s Tbin=%1.fs' % (self.title,tbin)
+		pdfname = 'ni%s_lc_for_lombscargle.pdf' % obsid
+		plot(lc_x,lc_y,lc_xerr,lc_yerr,xlabel='Time (sec)',ylabel='Count/bin',
+			title=title,pdfname=pdfname)
+		self.pdf_list.append(pdfname)
+
+		frequency, power = LombScargle(lc_x,lc_y).autopower(method='fast')
+		fmax=frequency[np.argmax(power)]
+		title = '%s Tbin=%.1fs' % (self.title,tbin)
+		pdfname = 'ni%s_lombscargle.pdf' % obsid
+		plot(frequency,power,xlabel='Frequency (Hz)',ylabel='Power',
+			xlog=True,ylog=True,title=title,pdfname=pdfname)		
+		self.pdf_list.append(pdfname)
+
+def run(obsid,yyyy_mm,flag_download=True,flag_pulsesearch=True):
+	outdir = 'ql%s' % obsid
+	cmd = 'rm -rf %s; mkdir -p %s' % (outdir,outdir)
+	print(cmd);os.system(cmd)
+	os.chdir(outdir)
+
 	if flag_download:
-		wget_nicer_cleanevt(obsid,yyyy_mm)
-	clevt_filename = 'ni%s_0mpu7_cl.evt.gz' % obsid
+		clevt_filename = wget_nicer_cleanevt(obsid,yyyy_mm)
+	else:
+		clevt_filename = 'ni%s_0mpu7_cl.evt.gz' % obsid
 	clevt = EVTFILE(clevt_filename)
 	clevt.show_property()
 	clevt.plot_curves_default()
-	clevt.plot_spectrum_gti()
+	clevt.plot_spectrum()
+	#clevt.plot_spectrum_gti()
+	if flag_pulsesearch:
+		orbfile = wget_nicer_orbfile(obsid,yyyy_mm)
+		clevt.run_barycorr(orbfile)
+		clevt.plot_LombScargle()
 	clevt.pdf_merge()			
+	os.chdir('../')
 
 if __name__ == '__main__':
 	obsid = sys.argv[1]
 	yyyy_mm = sys.argv[2]
 	run(obsid,yyyy_mm)
+	
 
 
