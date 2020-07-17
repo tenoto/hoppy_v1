5daa05d82a80d324fbb79d26ff1ecaf3880157ca
Teruaki Enoto <enoto@grpc.ad15.riken.go.jp>
2020-07-17 12:07:56 +0900

    extract xspec count rate

---
+++ hoppy/ftools/cli/extract_xspec_rate.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import os 
+import argparse
+import astropy.io.fits as fits
+
+if __name__=="__main__":
+
+	parser = argparse.ArgumentParser(
+		prog='extract_xspec_rate.py',
+		usage='extract_xspec_rate.py pha rmf arf emin emax',
+		description='Extract count rate from a xspec file.',
+		epilog='',
+		add_help=True,
+		)
+
+	parser.add_argument('pha',metavar='pha',type=str, help='xspec pha file.')
+	parser.add_argument('rmf',metavar='rmf',type=str, help='xspec rmf file.')	
+	parser.add_argument('arf',metavar='arf',type=str, help='xspec arf file.')		
+	parser.add_argument('emin',metavar='emin',type=float, help='energy min (keV).')			
+	parser.add_argument('emax',metavar='emax',type=float, help='energy max (keV).')				
+	args = parser.parse_args()	
+	#print(args.emin)			
+	#print(args.emax)				
+
+	cmd  = 'rm -f tmp_xspec.log tmp_rate.txt;\n'
+	cmd += "xspec <<EOF\n"
+	cmd += "data 1 %s\n" % args.pha
+	cmd += "resp 1 %s\n" % args.rmf
+	cmd += "arf 1 %s\n" % args.arf
+	cmd += "setplot energy\n"
+	cmd += "ignore 1:**-%.1f,%.1f-**\n" % (args.emin,args.emax)
+	cmd += 'pl ld\n'
+	cmd += 'log tmp_xspec.log\n'
+	cmd += 'show rate\n'
+	cmd += 'log none\n'
+	cmd += 'exit\n'
+	cmd += 'EOF\n'
+	#print(cmd);
+	os.system(cmd)
+
+	f = open('tmp_rate.txt','w')
+	cmd  = 'grep \'Net\' tmp_xspec.log | awk \'{print $7}\' > tmp_rate.txt;'
+	cmd += 'grep \'Net\' tmp_xspec.log | awk \'{print $9}\' >> tmp_rate.txt;'	
+	#print(cmd);
+	os.system(cmd)
+
+	with open('tmp_rate.txt', mode='rt', encoding='utf-8') as f:
+		read_data = list(f)
+	sub_rate  = float(read_data[0])
+	sub_error = float(read_data[1])
+	print("Count rate: %.6f" % sub_rate)
+	print("Rate error: %.6f" % sub_error)
+
+	cmd  = 'rm -f tmp_xspec.log tmp_rate.txt;\n'
+	#print(cmd);
+	os.system(cmd)
+
+
