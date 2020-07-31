e345b261ec5ddefa6657a8edbb8b7fad94483ee9
Teruaki Enoto <enoto@mumu.local>
2020-08-01 08:10:25 +0900

    add logfile for niwget.py

--- hoppy/nicer/cli/niwget.py
+++ hoppy/nicer/cli/niwget.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import os
+import time
+import datetime
 import argparse
 
 __author__ = 'Teruaki Enoto'
@@ -13,7 +15,7 @@ def get_parser():
 	"""
 	parser = argparse.ArgumentParser('niwget',
 		usage='%(prog)s -o obsid -y yyyy_mm ',
-		description='(example) %(prog)s.py -o 1012010152 -y 2018_05 '
+		description='(example) %(prog)s.py -o 1012010136 -y 2018_01 '
 		)
 	version = '%(prog)s ' + __version__
 	parser.add_argument('--version', '-v', action='version', version=version,
@@ -32,21 +34,36 @@ def run_wget(obsid, yyyy_mm):
 		cmd += 'https://heasarc.gsfc.nasa.gov/FTP/nicer/data/obs/%s//%s/%s/;\n' % (yyyy_mm, obsid, subdir)
 	print(cmd);os.system(cmd)
 
+	return(cmd)
+
+
+
 def main(args=None):
-	"""
-	Main entry point for your project.
-	Args:
-		args : list
-		A of arguments as if they were input in the command line. 
-		Leave it None to use sys.argv.
-    """
 	parser = get_parser()
 	args = parser.parse_args(args)
 
 	print("--niwget--")
 	print(" obsid: %s" % args.obsid)
 	print(" yyyy_mm: %s" % args.yyyy_mm)
-	run_wget(args.obsid, args.yyyy_mm)
+
+	start_time = time.time()
+	cmd = run_wget(args.obsid, args.yyyy_mm)
+	elapsed_time = time.time() - start_time
+
+	if not os.path.exists(args.obsid):
+		print("Error: file can not be download (check whether the yyyy_mm parameter is correct.)")
+		quit()
+
+	flog = '%s/wget.log' % args.obsid
+	f = open(flog,'w')
+	f.write('--niwget--\n')
+	f.write(" yyyy_mm: %s\n" % args.yyyy_mm)	
+	f.write(" obsid: %s\n" % args.obsid)	
+	f.write(' requried time for download (sec): %.1f\n' % elapsed_time)
+	f.write(' downloaded date: %s\n' % datetime.datetime.now())	
+	f.write('\n')
+	f.write(cmd)
+	f.close()
 
 if __name__=="__main__":
 	main()
